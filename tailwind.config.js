63cb0d0ac664785644924237c2876fa7112232e8
Nico-lab-cl <nicolas.cab.v@gmail.com>
2026-04-09 00:52:45 -0400

    Fix deployment: Stabilization with Expo 52 and React 18

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
