649295cf9587ab55a97ab3aeae6508a6bbda01bc
Nico-lab-cl <nicolas.cab.v@gmail.com>
2026-04-30 10:56:04 -0400

    Cleanup: Remove temporary debug scripts

--- tmp/list_pending.ts
+++
@@ -1,7 +0,0 @@
-
-import fs from 'fs';
-const data = JSON.parse(fs.readFileSync('tmp/query_results.json', 'utf8'));
-const pending = data.filter((l: any) => l.reservations[0].pie_status === 'PENDING');
-pending.forEach((l: any) => {
-  console.log(`Lote: ${l.number}, Propietario: ${l.reservations[0].name}, RUT: ${l.reservations[0].rut}`);
-});
